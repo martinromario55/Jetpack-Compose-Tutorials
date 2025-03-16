9674eaa19e1914370e78b38cef8e4852296caa5f
martinromario55 <martinromario55@gmail.com>
2025-03-16 23:00:47 +0300

    Gmail Clone Top Bar

---
+++ GmailClone/app/src/main/java/com/tuyiiya/gmailclone/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.tuyiiya.gmailclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tuyiiya.gmailclone.components.HomeAppBar
+import com.tuyiiya.gmailclone.ui.theme.GmailCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContent {
+            GmailCloneTheme {
+                Surface (color = MaterialTheme.colorScheme.background) {
+                    GmailApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GmailApp() {
+    Scaffold(
+        topBar = { HomeAppBar() }
+    ) {  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GmailCloneTheme {
+        GmailApp()
+    }
+}
