5771f00e10db692540e9319e37509a3f79904140
martinromario55 <martinromario55@gmail.com>
2025-03-28 10:11:32 +0300

    Composable Course - Instagram Profile UI

---
+++ InstagramProfileeUI/app/src/main/java/com/tuyiiya/instagramprofileeui/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.tuyiiya.instagramprofileeui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val UrlColor = Color(0xFF3D3D91)
+val Slate300 = Color(0xFF777777)
